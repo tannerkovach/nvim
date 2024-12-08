9eb6b86511d2e9acacd8588ad409a6ecf4584eaa
tannerkovach <kovach.tanner@gmail.com>
2024-12-08 12:53:50 -0800

    add api key to env variable

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -21,7 +21,7 @@ return {
       anthropic = function()
         return require('codecompanion.adapters').extend('anthropic', {
           env = {
-            api_key = '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',
+            api_key = vim.env.ANTHROPIC_API_KEY,
           },
         })
       end,
