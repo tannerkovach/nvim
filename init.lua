7be845413652ff46710b62d08c10311143b82a20
tannersltwtr <tanner@sltwtr.com>
2025-10-10 00:21:33 -0700

    hopefully fixed indentation issue with scss

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Options
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 vim.g.have_nerd_font = true
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,12 +6,11 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'liquid', 'javascript' },
+    ensure_installed = {'scss', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'liquid', 'javascript' },
     auto_install = true,
     highlight = {
       enable = true,
-      additional_vim_regex_highlighting = { 'ruby', 'scss' },
-    },
-    indent = { enable = { 'lua' } },
+      additional_vim_regex_highlighting = { 'ruby' },
+    }
   },
 }
